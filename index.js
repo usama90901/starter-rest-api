d14ef502f8af56d05e2cd1a5f60d77cb3a0e645f
usama90901 <150236205+usama90901@users.noreply.github.com>
2024-01-24 15:17:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const express = require('express')
-const app = express()
-const db = require('@cyclic.sh/dynamodb')
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+// MySQL Connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'ground_booking',
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL');
+});
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
